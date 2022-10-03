06989a9576139b742be13891f32e9153761c90ac
Tanay Kedar Kulkarni <tkulkarn@YLV70WHKYXmac.lan>
2022-10-03 13:14:32 -0500

    Refactored the wikipedia connector code

--- wikipedia_connector/wiki.py
+++ wikipedia_connector/wiki.py
@@ -1,6 +1,7 @@
 import wikipedia
 import pandas as pd
 import re
+import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
@@ -10,24 +11,6 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 import lxml
 import os
-
-parser = argparse.ArgumentParser(description="""Preprocessor""")
-parser.add_argument('-f','--topics', action='store', dest='topics', default="table,chalk,starcraft,predator", required=True, help="""wikipedia topics""")
-parser.add_argument('--project_dir', action='store', dest='project_dir',
-                        help="""--- For inner use of cnvrg.io ---""")
-parser.add_argument('--output_dir', action='store', dest='output_dir',
-                        help="""--- For inner use of cnvrg.io ---""")
-
-args = parser.parse_args()
-topic = args.topics
-cnvrg_workdir = os.environ.get("CNVRG_WORKDIR", "/cnvrg")
-if topic.endswith(".csv"):
-    topics = pd.read_csv(topic)
-    flag_0 = "dataframe"
-else:
-    topics = topic.split(",")
-    flag_0 = "list"
-
 
 class WikiPage:
     def __init__(self, page):
@@ -77,48 +60,71 @@ class WikiPage:
         text = re.sub(r'[^\x00-\x7F]+',' ', text)
         return text
 
-try:
-    if flag_0 == "dataframe":
-        input_list = []
-        list2 = []
-        disambiguation = 0
-        if(topics.shape[0]>0):
-            for i in range(topics.shape[0]):
-                abc1 = WikiPage(str(topics.iloc[i, 0]))
-                abc11 = abc1.get_wiki_page(str(topics.iloc[i, 0]))[0]
-                input_list.append(abc11)
-                if(abc1.get_wiki_page(str(topics.iloc[i, 0]))[1] == '1'):
-                    disambiguation = disambiguation + 1
-                list2.append(str(topics[i]))
-        else:
-            for j in range(len(topics.columns.values)):
-                abc2 = WikiPage(str(topics.columns.values[j]))
-                abc22 = abc2.get_wiki_page(str(topics.columns.values[j]))[0]
-                input_list.append(abc22)
-                if(abc2.get_wiki_page(str(topics.columns.values[j]))[1] == '1'):
-                    disambiguation = disambiguation + 1
-                list2.append(str(topics[i]))
-        #lst2 = ['x'] * len(input_list)
-        df1 = pd.DataFrame(list(zip(input_list, list2)), columns =['text', 'title'])
-        print('There were ' + str(disambiguation) + ' ambigious values in the input list')
-        output_summaries_file = "wiki_output.csv"
-        df1.to_csv(cnvrg_workdir+"/{}".format(output_summaries_file), index=False)
-    else:
-        input_list = []
-        list2 = []
-        disambiguation = 0
-        for i in range(len(topics)):
-            abc1 = WikiPage(str(topics[i]))
-            abc11 = abc1.get_wiki_page(str(topics[i]))[0]
-            input_list.append(abc11)
-            if(abc1.get_wiki_page(str(topics[i]))[1] == '1'):
-                disambiguation = disambiguation + 1
-            list2.append(str(topics[i]))
-        #lst2 = ['x'] * len(input_list)
-        df1 = pd.DataFrame(list(zip(input_list, list2)), columns =['text', 'title'])
-        print('There were ' + str(disambiguation) + ' ambigious values in the input list')
-        output_summaries_file = "wiki_output.csv"
-        df1.to_csv("{}".format(output_summaries_file), index=False)
 
-except (wikipedia.exceptions.PageError, wikipedia.exceptions.WikipediaException,urllib.error.HTTPError):
-    print('Does not match any page. Try another ID next time')
+def wiki_main():
+    parser = argparse.ArgumentParser(description="""Preprocessor""")
+    parser.add_argument('-f','--topics', action='store', dest='topics', default="table,chalk,starcraft,predator", required=True, help="""wikipedia topics""")
+    parser.add_argument('--project_dir', action='store', dest='project_dir',
+                        help="""--- For inner use of cnvrg.io ---""")
+    parser.add_argument('--output_dir', action='store', dest='output_dir',
+                        help="""--- For inner use of cnvrg.io ---""")
+
+    args = parser.parse_args()
+    topic = args.topics
+    cnvrg_workdir = os.environ.get("CNVRG_WORKDIR", "/cnvrg")
+    if topic.endswith(".csv"):
+        topics = pd.read_csv(topic)
+        flag_0 = "dataframe"
+    else:
+        topics = topic.split(",")
+        flag_0 = "list"
+
+
+    try:
+        if flag_0 == "dataframe":
+            input_list = []
+            list2 = []
+            disambiguation = 0
+            if(topics.shape[0]>0):
+                for i in range(topics.shape[0]):
+                    abc1 = WikiPage(str(topics.iloc[i, 0]))
+                    abc11 = abc1.get_wiki_page(str(topics.iloc[i, 0]))[0]
+                    input_list.append(abc11)
+                    if(abc1.get_wiki_page(str(topics.iloc[i, 0]))[1] == '1'):
+                        disambiguation = disambiguation + 1
+                    list2.append(str(topics[i]))
+            else:
+                for j in range(len(topics.columns.values)):
+                    abc2 = WikiPage(str(topics.columns.values[j]))
+                    abc22 = abc2.get_wiki_page(str(topics.columns.values[j]))[0]
+                    input_list.append(abc22)
+                    if(abc2.get_wiki_page(str(topics.columns.values[j]))[1] == '1'):
+                        disambiguation = disambiguation + 1
+                    list2.append(str(topics[i]))
+            #lst2 = ['x'] * len(input_list)
+            df1 = pd.DataFrame(list(zip(input_list, list2)), columns =['text', 'title'])
+            print('There were ' + str(disambiguation) + ' ambigious values in the input list')
+            output_summaries_file = "wiki_output.csv"
+            df1.to_csv(cnvrg_workdir+"/{}".format(output_summaries_file), index=False)
+        else:
+            input_list = []
+            list2 = []
+            disambiguation = 0
+            for i in range(len(topics)):
+                abc1 = WikiPage(str(topics[i]))
+                abc11 = abc1.get_wiki_page(str(topics[i]))[0]
+                input_list.append(abc11)
+                if(abc1.get_wiki_page(str(topics[i]))[1] == '1'):
+                    disambiguation = disambiguation + 1
+                list2.append(str(topics[i]))
+            #lst2 = ['x'] * len(input_list)
+            df1 = pd.DataFrame(list(zip(input_list, list2)), columns =['text', 'title'])
+            print('There were ' + str(disambiguation) + ' ambigious values in the input list')
+            output_summaries_file = "wiki_output.csv"
+            df1.to_csv("{}".format(output_summaries_file), index=False)
+
+    except (wikipedia.exceptions.PageError, wikipedia.exceptions.WikipediaException,urllib.error.HTTPError):
+        print('Does not match any page. Try anßßother ID next time')
+
+if __name__ == '__main__':
+    wiki_main()
