31d93312898e496bca90b3db1c88f3c5f292358f
Tanay Kedar Kulkarni <tanay.kedar.kulkarni@intel.com>
2022-10-10 15:57:15 -0500

    Refactored wikipedia unittest code

--- wikipedia_connector/test_wiki.py
+++ wikipedia_connector/test_wiki.py
@@ -6,16 +6,19 @@ import urllib.request
 from urllib.request import urlopen
 
 class TestWiki(unittest.TestCase):
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(self) -> None:
         self.args = []
         with open("arguments.txt") as f:
             self.args.append(f.readline())
         self.wiki = WikiPage(str(self.args[0]))
 
         # Extracting raw text - for 'get_clean_text' argument
-        soup = BeautifulSoup(urlopen('https://en.wikipedia.org/wiki/'+str(self.args[0])).read(), 'lxml')
-        self.raw_text = ''
-        for paragraph in soup.find_all('p'):
+        soup = BeautifulSoup(
+            urlopen("https://en.wikipedia.org/wiki/" + str(self.args[0])).read(), "lxml"
+        )
+        self.raw_text = ""
+        for paragraph in soup.find_all("p"):
             self.raw_text += paragraph.text
 
         # Setup for testing 'get_wiki_page' and 'get_clean_text' functions
@@ -27,38 +30,33 @@ class TestWiki(unittest.TestCase):
             self.content_val = f.read()
         self.content_val = ' ' + self.content_val + ' '
 
-class TestGetWikiPage(TestWiki):
-    def test_return_type(self):
-        '''Checks if the function returns list'''
+    def test_return_type_list(self):
+        # Checks if the 'get_wiki_page' function returns list
+        self.assertIsInstance(self.wiki_output, list)
+
+    def test_str_return_type(self):
+        # Checks if the list object type is string
         self.assertIsInstance(
-            self.wiki_output, list
+            self.wiki_output[0], str
         )
 
     def test_output_length(self):
-        '''Checks if the get_wiki_page function returns an output list with length 2'''
+        # Checks if the get_wiki_page function returns an output list with length 2
         self.assertEqual(
             len(self.wiki_output), 2
         )
 
-    def test_str_return_type(self):
-        '''Checks if the list object type is string'''
-        self.assertIsInstance(
-            self.wiki_output[0], str
-        )
-    
-    # @unittest.skip('WIP') - get the actual output to compare from the developer (Understand the csv output and the function output)
     def test_text_content(self):
-        '''Checks the content validity'''
+        # Checks the content validity
         self.assertEquals(
             self.wiki_output[0], self.content_val
         )
-        
-class TestGetCleanText(TestWiki):
-    def test_return_type(self):
-        '''Checks if the function returns string'''
-        self.assertIsInstance(
-            self.processed_text, str
-        )
 
-if __name__ == '__main__':
+    def test_return_type_str(self):
+        # Checks if the 'get_clean_text' function returns string
+        self.assertIsInstance(self.processed_text, str)
+
+    # WIP - Develop a test-case to check 'get_clean_text' functionality
+
+if __name__ == "__main__":
     unittest.main()
