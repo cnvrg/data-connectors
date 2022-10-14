2272b1ba9c18a74cab9907e5191ceb7f34f549bd
Kris Pan <repoofideas@github.com>
2022-10-14 12:05:26 -0500

    3 unit tests tested

--- influxdb_connector/README.md
+++ influxdb_connector/README.md
@@ -4,8 +4,10 @@
 - `--url` - string, required. Client url for your InfluxDB workspace. For security reasons, we recommend creating/updating the Cnvrg Secret `INFLUXDB_URL` under Project->Settings->Secrets to store the InfluxDB Client ID. Enter the value ‘Secret’ if the Cnvrg Secret has been created/updated; otherwise enter the actual Client ID.
 - `--org` - string, required. Organization email for your InfluxDB workspace. For security reasons, we recommend creating/updating the Cnvrg Secret `INFLUXDB_ORG` under Project->Settings->Secrets to store the InfluxDB Client ID. Enter the value ‘Secret’ if the Cnvrg Secret has been created/updated; otherwise enter the actual Client ID.
 - `--bucket` - string, required. Data bucket to pull data from. This can also be stored as Cnvrg Secret `INFLUXDB_BUCKET` under Project->Settings->Secrets to store the InfluxDB Client ID. Enter the value ‘Secret’ if the Cnvrg Secret has been created/updated; otherwise enter the actual Client ID.
-- `--range_start` - string, optional. Fetch from starting date in the format of 2020-01-01T00:00:00Z, or specify -1d, -1h, -1m for last day, hour, or minute. For detailed query language, refer to [Influxdb Doc](https://docs.influxdata.com/influxdb/cloud/query-data/flux/Default) value - '2020-01-01T00:00:00Z'
+- `--measurement` - string, required. Data measurement to pull data from. 
+- `--time_col` - string, required. Name of the time column. 
+- `--range_start` - string, optional. Fetch from starting date in the format of 2020-01-01T00:00:00Z, or specify -1y -1d, -1h, -1m for year, day, hour, or minute. For detailed query language, refer to [Influxdb Doc](https://docs.influxdata.com/influxdb/cloud/query-data/flux/Default)
+- `--range_end` - string, optional. Fetch from starting date in the format of 2020-01-01T00:00:00Z, or now() for current datetime. For detailed query language, refer to [Influxdb Doc](https://docs.influxdata.com/influxdb/cloud/query-data/flux/Default)
 - `--cnvrg_dataset` - string, optional. Name of the cnvrg dataset to store the csv file. Default value - ‘None’
-<<<<<<< .merge_file_8q8hqp
 - `--file_name` - string, optional. Name of the csv file to be generated. Default value - influxdb.csv’
 
--- influxdb_connector/influxdb_connector.py
+++ influxdb_connector/influxdb_connector.py
@@ -3,8 +3,7 @@ import argparse
 import pandas as pd
 from collections import defaultdict
 from cnvrgv2 import Cnvrg
-import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 cnvrg_workdir = os.environ.get("CNVRG_WORKDIR", "/cnvrg")
@@ -42,12 +41,34 @@ def parse_parameters():
         help="""--- bucket where the data is pulled from ---""",
     )
     parser.add_argument(
+        "--measurement",
+        action="store",
+        dest="measurement",
+        required=True,
+        help="""--- measurement name where the data is pulled from ---""",
+    )
+    parser.add_argument(
+        "--time_col",
+        action="store",
+        dest="time_col",
+        required=True,
+        help="""--- name of the time column, which will be used for indexing ---""",
+    )
+    parser.add_argument(
         "--range_start",
         action="store",
         dest="range_start",
         required=False,
         default="-10y",
-        help="""--- fetch from starting date in the format of 2020-01-01T00:00:00Z, or specify -1d, -1h, -1m for last day, hour, or minute ---""",
+        help="""--- fetch from starting datetime in the format of 2020-01-01T00:00:00Z, or specify -1d, -1h, -1m for last day, hour, or minute ---""",
+    )
+    parser.add_argument(
+        "--range_end",
+        action="store",
+        dest="range_end",
+        required=False,
+        default="now()",
+        help="""--- fetch through ending datetime in the format of 2020-01-01T00:00:00Z, or defaults to now() for current time ---""",
     )
     parser.add_argument(
         "--local_dir",
@@ -75,58 +96,123 @@ def parse_parameters():
     )
     return parser.parse_args()
 
+
+# custom exceptions
 class NoneCnvrgDatasetError(Exception):
     """Raise if message is None object"""
+
     def __init__(self):
         super().__init__()
 
     def __str__(self):
         return "NoneMessageError: Please ensure the input is valid!"
 
-def influxdb_query(url, token, org, bucket, range_start):
-    """
-    Creates dictionary from query of given inputs
-        Args:
-            url: https address of influxdb cloud
-            token: token for accessing influxdb cloud
-            org: email address for user authentication
-            bucket: name of the bucket to access
-            range_start: starting range for query, default set to last 10 years of data
-    Returns:
-        A dictionary containing time and field columns from influxdb query
-    """
-    client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-    query_api = client.query_api()
-    if range_start.lower() != "none":
-        query = 'from(bucket: "' + bucket + '")\n |> range(start:' + range_start + ")"
-    else:
-        query = 'from(bucket: "' + bucket + '")\n |> range(start:-10y)'
-    tables = query_api.query(query, org=org)
-    csv_builder = dict()
-    csv_builder["time"] = []
 
-    # build dictionary from query
-    for table in tables:
-        for record in table.records:
-            # scrape all fields from query results
-            if record["_field"] not in csv_builder:
-                csv_builder[record["_field"]] = []
-            csv_builder[record["_field"]].append(record["_value"])
-            csv_builder["time"].append(record["_time"])
-    # since time is shown for every field, remove redundancy in time
-    time_len = len(csv_builder["time"]) // (len(csv_builder) - 1)
-    csv_builder["time"] = csv_builder["time"][:time_len]
-    
-    # truncate datetime string so that the +00:00 is removed 
-    sliced_time = []
-    for t in csv_builder["time"]:
-        sliced_time.append(str(t)[:19])
-    csv_builder["time"] = sliced_time
+class EmptyDataError(Exception):
+    """Raise if there is no data"""
 
-    return csv_builder
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "EmptyDataError: The query resulted in empty data, please check if the bucket is empty"
+
+
+class IncorrectDirectoryError(Exception):
+    """Raise if directory is invalid"""
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "IncorrectDirectoryError: Please ensure the input directory is valid!"
+
+
+class Influxdb:
+    def __init__(self, url, token, org):
+        self.client = InfluxDBClient(url=url, token=token, org=org)
+
+    def ping(self):
+        """
+        Returns bool depending on the status of connection
+        Returns:
+            True if connection is established, else false
+        """
+        return self.client.ping()
+
+    def get_data(
+        self,
+        org,
+        bucket,
+        measurement,
+        time_col,
+        range_start=None,
+        range_end=None,
+        verbose=False,
+    ):
+        """
+        Creates pandas dataframe from query of given inputs. Column order maybe different
+            Args:
+                org: email address for user authentication
+                bucket: name of the bucket
+                measurement: name of the measurement, if you used the influxdb_write_tester.ipynb, the default as sensors
+                time_col: name of the column used as timestamps
+                range_start: starting range for query, default set to last 10 years
+                range_end: ending range for query, default to current datetime
+        Returns:
+            A pandas dataframe containing time and field entries from influxdb query
+        """
+        # default arguments if no input was given
+        if str(range_start).lower() == "none":
+            range_start = "-10y"
+        if str(range_end).lower() == "none":
+            range_end = "now()"
+        # custom query in flux
+        query_api = self.client.query_api()
+
+        query = f"""from(bucket: "{bucket}")
+        |> range(start: {range_start}, stop: {range_end}) 
+        |> filter(fn: (r) => r._measurement == "{measurement}") 
+        |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+        """
+        data = query_api.query_data_frame(query=query, org=org)
+
+        # convert back the time column name
+        if "_time" in data.columns:
+            data.rename(columns={"_time": time_col}, inplace=True)
+        # remove meta data columns and timezone utc from datetime
+        if not verbose:
+            meta_columns = ["result", "table", "_start", "_stop", "_measurement"]
+            data = data.drop(meta_columns, axis=1)
+            # if you need to preserve timezone utc on timestamps, remove the line below
+            data[time_col] = pd.to_datetime(data[time_col]).dt.tz_convert(None)
+        return data
+
+    def write_data(self, file_dir, time_col, bucket, measurement, rows_to_write=None):
+        """
+        Write data to specific bucket and measurement
+            Args:
+                file_dir: file directory of the csv file to write
+                time_col: name of the time column
+                bucket: name of the bucket
+                measurement: name of the measurement, if you used the influxdb_write_tester.ipynb, the default as sensors
+                rows_to_write: integer of num of rows to write, mostly for testing purpose
+        """
+        df = pd.read_csv(file_dir, sep=",", engine="python", index_col=False)
+        df[time_col] = pd.to_datetime(df[time_col])
+        if rows_to_write != None:
+            df = df.head(rows_to_write)
+        self.client.write_api(write_options=SYNCHRONOUS).write(
+            bucket=bucket,
+            record=df,
+            data_frame_measurement_name=measurement,
+            data_frame_timestamp_column=time_col,
+        )
+
 
 def main():
     args = parse_parameters()
+    # grab secret
     if args.token.lower() == "secret":
         args.token = os.environ.get("INFLUXDB_TOKEN")
     if args.url.lower() == "secret":
@@ -135,17 +221,25 @@ def main():
         args.org = os.environ.get("INFLUXDB_ORG")
     if args.bucket.lower() == "secret":
         args.bucket = os.environ.get("INFLUXDB_BUCKET")
-    # return dictionary from custom query
-    csv_builder = influxdb_query(
-        args.url, args.token, args.org, args.bucket, args.range_start
+    # check if directory is valid
+    if str(args.cnvrg_dataset).lower() != "none":
+        if not os.path.exists(args.local_dir):
+            raise IncorrectDirectoryError()
+    # return pandas dataframe from custom query
+    influxdb = Influxdb(url=args.url, token=args.token, org=args.org)
+    df = influxdb.get_data(
+        args.org,
+        args.bucket,
+        args.measurement,
+        args.time_col,
+        args.range_start,
+        args.range_end,
+        verbose=False,
     )
-
-    # build pandas dataframe for csv
-    df = pd.DataFrame(csv_builder)
     df.to_csv(args.local_dir + "/" + args.file_name, index=False)
 
     # Store csv as cnvrg dataset
-    if args.cnvrg_dataset.lower() != 'none':
+    if str(args.cnvrg_dataset).lower() != "none":
         cnvrg = Cnvrg()
         ds = cnvrg.datasets.get(args.cnvrg_dataset)
         try:
