916294e8db7b08dd12b63793d550bb83ba9402dd
Tanay Kedar Kulkarni <tanay.kedar.kulkarni@intel.com>
2022-10-25 15:07:39 -0500

    Refactored snowflake unit-testing code to add config yamls

--- snowflake_connector/test_snowflake.py
+++ snowflake_connector/test_snowflake.py
@@ -3,36 +3,38 @@ import unittest
 import os
 import sys
 import pandas
+import yaml
+from yaml.loader import SafeLoader
 from snowflake_connector import connect, to_csv, run, close_connection, to_df
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+YAML_ARG_TO_TEST = "test_arguments"
 
 class test_snowflake_connector(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         # Parse the snowflake parameters
-        self.args = []
-        with open("arguments.txt") as f:
-            for line in f:
-                self.args.append(line[:-1])
+        cfg_path = os.path.dirname(os.path.abspath(__file__))
+        cfg_file = cfg_path + "/" + "test_config.yaml"
+        self.test_cfg = {}
+        with open(cfg_file) as c_info_file:
+            self.test_cfg = yaml.load(c_info_file, Loader=SafeLoader)
+        self.test_cfg = self.test_cfg[YAML_ARG_TO_TEST]
 
-        # Store the account credentails for further testing
-        self.user = self.args[0]
-        self.password = self.args[1]
-        self.account = self.args[2]
-        self.database = self.args[3]
-        self.query = self.args[4]
-        self.output_file = self.args[5]
-        self.warehouse = ''
-        self.schema = ''
-        self.incorrect_password = 'garbage'
+        # Make a Unit-testing data directory    
         self.unittest_dir = "unit_test_data"
         self.local_dir = os.path.dirname(os.path.abspath(__file__))
-        # Make a Unit-testing data directory
         self.data_path = os.path.join(self.local_dir, self.unittest_dir)
         os.mkdir(self.data_path)
 
         # Intialize connection for 'query' testing
-        self.snowflake_connection = connect(self.password, self.warehouse, self.account, self.user, self.database, self.schema)
+        self.snowflake_connection = connect(
+            self.test_cfg["password"], 
+            self.test_cfg["warehouse"], 
+            self.test_cfg["account"], 
+            self.test_cfg["user"], 
+            self.test_cfg["database"], 
+            self.test_cfg["schema"]
+            )
 
     @classmethod
     def tearDownClass(self):
@@ -46,7 +48,13 @@ class test_snowflake_connector(unittest.TestCase):
     # Test snowflake connection exception
     def test_connection_exception(self):
         self.assertRaises(
-            SystemExit, connect, self.incorrect_password, self.warehouse, self.account, self.user, self.database, self.schema
+            SystemExit, connect, 
+            self.test_cfg["incorrect_password"], 
+            self.test_cfg["warehouse"], 
+            self.test_cfg["account"], 
+            self.test_cfg["user"], 
+            self.test_cfg["database"], 
+            self.test_cfg["schema"]
         )
 
     # Test snowflake close connection exception
@@ -58,7 +66,7 @@ class test_snowflake_connector(unittest.TestCase):
     # Test snowflake run query 
     def test_run_query(self):
         self.assertTrue(
-            run(self.snowflake_connection, self.query)
+            run(self.snowflake_connection, self.test_cfg["query"])
         )
 
     # Test snowflake run query exception
@@ -70,12 +78,17 @@ class test_snowflake_connector(unittest.TestCase):
     # Test data-frame output 
     def test_df_output(self):
         self.assertIsInstance(
-            to_df(self.snowflake_connection, self.query), pandas.core.frame.DataFrame
+            to_df(self.snowflake_connection, self.test_cfg["query"]), pandas.core.frame.DataFrame
         )
 
     # Test csv output - skip this test if path is set to '/cnvrg'
     def test_csv_output(self):
-        self.assertTrue(str(type(to_csv(self.data_path, self.snowflake_connection, self.query, self.output_file))), "_csv.reader")
+        self.assertTrue(str(type(to_csv(
+            self.data_path, 
+            self.snowflake_connection, 
+            self.test_cfg["query"], 
+            self.test_cfg["output_file"]
+            ))), "_csv.reader")
 
 if __name__ == '__main__':
     unittest.main()
