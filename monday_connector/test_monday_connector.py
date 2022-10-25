3ae0cfaa43faf4e6131ce08d6d5f0003a8535dbd
Tanay Kedar Kulkarni <tanay.kedar.kulkarni@intel.com>
2022-10-25 16:36:14 -0500

    Refactored monday unittests to add config files, imporved infra

--- monday_connector/test_monday_connector.py
+++ monday_connector/test_monday_connector.py
@@ -2,62 +2,60 @@ import unittest
 import json
 import os, sys, shutil
 import _csv
+import yaml
+from yaml.loader import SafeLoader
 from monday_connector import boards_and_workspaces, boards, specificquery
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+YAML_ARG_TO_TEST = "test_arguments"
 
 class test_monday_connector(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         # Parse the monday connector arguments 
-        self.args = []
-        with open("arguments.txt") as f:
-            for line in f:
-                self.args.append(line[:-1])
+        cfg_path = os.path.dirname(os.path.abspath(__file__))
+        cfg_file = cfg_path + "/" + "test_config.yaml"
+        self.test_cfg = {}
+        with open(cfg_file) as c_info_file:
+            self.test_cfg = yaml.load(c_info_file, Loader=SafeLoader)
+        self.test_cfg = self.test_cfg[YAML_ARG_TO_TEST]
 
-        # Store account credentails 
-        self.api_url = "https://api.monday.com/v2"
-        self.api_key = self.args[0]
-        self.headers = {"Authorization": self.api_key}
-        self.specific_query = self.args[1]
-        self.invalid_query = ''
+        # Internal parameters for functions 
+        self.headers = {"Authorization": self.test_cfg["api_key"]}
         self.sep_cols = ''
         self.equiv_cols = ''
         self.not_flat = ''
+
+        # Make a Unit-testing data directory
         self.unittest_dir = "unit_test_data"
         self.local_dir = os.path.dirname(os.path.abspath(__file__))
-        # Make a Unit-testing data directory
         self.data_path = os.path.join(self.local_dir, self.unittest_dir)
         os.mkdir(self.data_path)
 
         # Calling boards and workspaces - retrieve board ids 
-        self.board_ids = boards_and_workspaces(self.api_url, self.headers, self.data_path)
+        self.board_ids = boards_and_workspaces(self.test_cfg["api_url"], self.headers, self.data_path)
 
     # Skip or comment this out if you need to have a look at the data 
     @classmethod
     def tearDownClass(self):
         shutil.rmtree(self.data_path)
 
-    '''
-    Globally defined vairables in the script - apiUrl and headers - prevents successful unittesting 
-    boards_and_workspaces violates the single responsibilty principle - doing multiple things 
-    Re-factored the code to include arguments and reduce the dependency on global variables
-    '''
+    # Check the list object
     def test_list_output(self):
         self.assertTrue(self.board_ids, list)
 
     # Check csv output for boards function 
     def test_csv_output(self):
-        self.assertTrue(str(type(boards([self.board_ids[0]], self.api_url, self.headers, self.sep_cols, self.equiv_cols, self.not_flat, self.data_path))), "_csv.reader")
+        self.assertTrue(str(type(boards([self.board_ids[0]], self.test_cfg["api_url"], self.headers, self.sep_cols, self.equiv_cols, self.not_flat, self.data_path))), "_csv.reader")
 
     # Test exception for 'specificquery' function - passing invalid query
     def test_monday_connection_exception(self):
         self.assertRaises(
-            Exception, specificquery, self.invalid_query, self.api_url, self.api_key, self.data_path
+            Exception, specificquery, self.test_cfg["invalid_query"], self.test_cfg["api_url"], self.test_cfg["api_key"], self.data_path
         )
 
     # Test Json output 'specific.json' and see if it loads properly 
     def test_JSON_output(self):
-        specificquery(self.specific_query, self.api_url, self.headers, self.data_path)
+        specificquery(self.test_cfg["specific_query"], self.test_cfg["api_url"], self.headers, self.data_path)
         with open(self.data_path + "/" + "specific.json", 'r') as f:
             json_data = json.load(f)
         f.close()
